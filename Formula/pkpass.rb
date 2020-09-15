84f664578fef326c9dadb8553dd659f24dc99353
GitHub Action <action@github.com>
2020-09-15 12:06:27 +0000

    updating package_info

--- Formula/pkpass.rb
+++ Formula/pkpass.rb
@@ -9,8 +9,8 @@ class Pkpass < Formula
   depends_on "python"
 
   resource "cffi" do
-    url "https://files.pythonhosted.org/packages/f7/09/88bbe20b76ca76be052c366fe77aa5e3cd6e5f932766e5597fecdd95b2a8/cffi-1.14.2.tar.gz"
-    sha256 "ae8f34d50af2c2154035984b8b5fc5d9ed63f32fe615646ab435b05b132ca91b"
+    url "https://files.pythonhosted.org/packages/cb/ae/380e33d621ae301770358eb11a896a34c34f30db188847a561e8e39ee866/cffi-1.14.3.tar.gz"
+    sha256 "f92f789e4f9241cd262ad7a555ca2c648a98178a953af117ef7fad46aa1d5591"
   end
 
   resource "colored" do
